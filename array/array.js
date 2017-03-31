32d6212eec54664064c20781e2e5fbb7aa7bb7d8
ximet <dq.ximet@gmail.com>
2017-03-31 18:21:42 +0300

    add sub method need test

--- array/array.js
+++ array/array.js
@@ -13,6 +13,9 @@ const reduce = f => y => ([x, ...xs]) => {
 const reduce = f => Y(g => y => xs => length(xs) < 1 ? y : g(f(y)(head(xs)))(tail(xs)));
 
 const map = f => xs => xs.map(f); //for carryng
+const cons = a => b => [a, ...b];
+const build = g => g(cons)([]);
+
 
 module.exports = {
   reduce, map
